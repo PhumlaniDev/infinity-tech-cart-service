debf20ac3eab26d6ad2636ba99a0945aeeafdc44
Phumlani Arendse <aphumlani.dev@gmail.com>
2026-02-10 11:51:17 +0200

    refactor CartController to remove unnecessary exceptions in addProductToCart method

--- src/main/java/com/phumlanidev/cartservice/controller/CartController.java
+++ src/main/java/com/phumlanidev/cartservice/controller/CartController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.ExecutionException;
-
 /**
  * Comment: this is the placeholder for documentation.
  */
@@ -35,7 +33,7 @@ public class CartController {
    * Comment: this is the placeholder for documentation.
    */
   @PostMapping("/add")
-  public ResponseEntity<Void> addProductToCart(@Valid Long productId, Integer quantity) throws ExecutionException, InterruptedException {
+  public ResponseEntity<Void> addProductToCart(@Valid Long productId, Integer quantity) {
     cartService.addProductToCart(productId, quantity);
     return ResponseEntity.ok().build();
   }
